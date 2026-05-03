c77629e5ffafda07e8b2205fe6665df94a6b3a2b
Dierumox <dierumox@gmail.com>
2026-05-03 18:25:43 +0200

    Mathematical foundation cplusplus update

--- Mathematical_foundation_cplusplus/Mathematical_foundation_cplusplus.ino
+++ Mathematical_foundation_cplusplus/Mathematical_foundation_cplusplus.ino
@@ -3,21 +3,12 @@
 double rx1 = 0;
 double ry1 = 2;
 double rz1 = 0;
-<<<<<<< HEAD:Mathematical_foundation_cplusplus/Mathematical_foundation_cplusplus.ino
 double rx2 = -1;
 double ry2 = -1.5;
 double rz2 = 0;
 double rx3 = 1;
 double ry3 = -1.5;
 double rz3 = 0;
-=======
-double rx2 = -0.1;
-double ry2 = -0.15;
-double rz2 = 0.05;
-double rx3 = 0.1;
-double ry3 = -0.15;
-double rz3 = 0.05;
->>>>>>> 8401df00eb466b1f16568287afca283a8f4823b8:Mathematical_foundation_cplusplus.ino
 double b [6] [1] = {
   {0},
   {0},
@@ -223,7 +214,7 @@ void loop() {
 
   //***// PRINTING OF AAp ON THE SERIAL MONITOR //***//
 
-  Serial.println("Matrix AAp (be almost a I_6):");
+  Serial.println("Matrix AAp (it must be almost I_6):");
   for(int i = 0; i < 6; i++){
     for(int j = 0; j < 6; j++){
       if(j < 5) Serial.print(AAp [i] [j]) && Serial.print(", ");
