f4794e11aabcaf58e879f32b336641b4d02fdbe9
Volodishlav <sarukunjc@gmail.com>
2026-05-21 13:52:54 +0200

    add clarke function

--- Gimbal design/Gimbal design - 7.1/FOC algorithm/Forward Park and Clarke Transformations_cplusplus.cpp
+++ Gimbal design/Gimbal design - 7.1/FOC algorithm/Forward Park and Clarke Transformations_cplusplus.cpp
@@ -1,1 +1,17 @@
 #include <math.h>
+
+struct cp_variables {
+    float I_alpha;
+    float I_beta;
+    float I_a = 1; // igualar a lectura ADC  fase A
+    float I_b = 0.5; // igualar a lectura ADC  fase B
+}
+
+
+inline float clarkepark[] (cp_variables) {
+    I_alpha = I_a
+    I_beta = (2 * I_b + I_a) / sqrt(3);
+    return 
+
+    // park 
+}
